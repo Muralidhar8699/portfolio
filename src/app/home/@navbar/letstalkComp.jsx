592045d34681abdd391d932a48335f0975b33d5d
Murali dhar <muralidhar8699@email.com>
2024-04-05 18:22:11 +0530

    added the netlify

--- src/app/home/@navbar/letstalkComp.jsx
+++ src/app/home/@navbar/letstalkComp.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 export default function LetstalkComp() {
   return (
     <div className={styles.main_div}>
-      <button className={styles.talk_btn}>Let's Talk</button>
+      <button className={styles.talk_btn}>Let&#39;s Talk</button>
       <button className={styles.arrow_btn}>
         <GoArrowUp className={styles.arrow} />
       </button>
