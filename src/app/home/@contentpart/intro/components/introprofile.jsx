8e4c45b2729727fb1696d95612d4ede217f6ac00
Murali dhar <muralidhar8699@email.com>
2024-04-07 17:31:33 +0530

    changes applied

--- src/app/home/@contentpart/intro/components/introprofile.jsx
+++ src/app/home/@contentpart/intro/components/introprofile.jsx
@@ -5,7 +5,7 @@ import styles from '../Introstyles/introprofile.module.scss'
 export default function Introprofile() {
   return (
     <div className={styles.profile_div}>
-      11111111111111
+      1
     </div>
   )
 }
