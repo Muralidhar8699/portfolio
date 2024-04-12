7cf66a46bb3cf24e6437c70cad41532f28fd8ef6
Murali dhar <muralidhar8699@email.com>
2024-04-12 15:18:35 +0530

    changes applied

--- src/app/home/@contentpart/projects/page.jsx
+++ src/app/home/@contentpart/projects/page.jsx
@@ -5,11 +5,11 @@ import { useRouter, usePathname } from "next/navigation";
 export default function ProjectsPage() {
   const pathname = usePathname();
   const router = useRouter();
-  if (pathname === "/home/projects") {
-    console.log(pathname);
-    useEffect(() => {
-      router.push("/home/projects/ogami");
-    });
-  }
+  // if (pathname === "/home/projects") {
+  //   console.log(pathname);
+  //   useEffect(() => {
+  //     router.push("/home/projects/ogami");
+  //   });
+  // }
   return <section>{/* <Projectslayout /> */}</section>;
 }
